2b843c0d59d5c2e864cd12355539650fe6eb53aa
Dustin <dustinhollowaypdx@gmail.com>
2025-04-15 06:59:06 -0700

    Second commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  basePath: "/Streamline-AV", // 👈 Use your repo name here
+  trailingSlash: true, // 👈 Ensures links resolve correctly on GitHub Pages
+};
 
 export default nextConfig;
